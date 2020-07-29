4ec3a6cf7cf5939f33c4e20e44027fc11c06a73c
Mark Wagdy <16P8146@eng.asu.edu.eg>
2020-07-29 16:17:23 +0200

    Updates

---
+++ precache-manifest.fee84ffefe1bb7cc6b201e5a134ffa65.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd2c8aa8a607f1545ecd02a7a282a186",
+    "url": "/MyDoctor/index.html"
+  },
+  {
+    "revision": "c586bfc8ca6d831a021e",
+    "url": "/MyDoctor/static/css/main.18e05f4c.chunk.css"
+  },
+  {
+    "revision": "08a3ba5b28aea9f51073",
+    "url": "/MyDoctor/static/js/2.0292a1e1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/MyDoctor/static/js/2.0292a1e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c586bfc8ca6d831a021e",
+    "url": "/MyDoctor/static/js/main.c66dc985.chunk.js"
+  },
+  {
+    "revision": "0be8a62159a24393a117",
+    "url": "/MyDoctor/static/js/runtime-main.d24f4491.js"
+  },
+  {
+    "revision": "6ec90edb96a8078e9322dfa1f7a1c3ab",
+    "url": "/MyDoctor/static/media/doctor (1).6ec90edb.svg"
+  },
+  {
+    "revision": "d2d84a1de4bce0fc6654db1f305bc8d3",
+    "url": "/MyDoctor/static/media/healthcare-medical-concept-medicine-doctor-with-stethoscope-hand-patients-come_34200-313.d2d84a1d.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/MyDoctor/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c486456879fdb92dfded8be38617ebd6",
+    "url": "/MyDoctor/static/media/thumb-1920-513905.c4864568.jpg"
+  }
+]);
